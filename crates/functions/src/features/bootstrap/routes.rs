5f40a6ce4c058dc17539753623e9928741ab40c1
NsdHSO <nechiforelsamuel@gmail.com>
2026-04-29 00:18:02 +0300

    refactor: remove bootstrap feature

--- crates/functions/src/features/bootstrap/routes.rs
+++
@@ -1,7 +0,0 @@
-use actix_web::web;
-
-use super::handlers;
-
-pub fn configure_bootstrap(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/bootstrap").route(web::post().to(handlers::bootstrap)));
-}
